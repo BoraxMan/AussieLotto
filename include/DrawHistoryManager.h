517592832f1f02bb1315b13ad6ca86eee35c512b
Dennis Katsonis <dennisk@netspace.net.au>
2015-12-09 23:24:48 +1100

    Added DrawHistorManager GUI.
    New FLUID created GUI to manage results history.

---
+++ include/DrawHistoryManager.h
@@ -0,0 +1,29 @@
+#ifndef DRAWHISTORYMANAGER_H
+#define DRAWHISTORYMANAGER_H
+#include <FL/Fl.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Scroll.H>
+#include <FL/Fl_Tile.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Float_Input.H>
+#include <Fl/Fl_Choice.h>
+#include <FL/Fl_File_Chooser.H>
+#include <string>
+
+#include "DrawHistoryManagerGUI.h"
+#include "ResultManager.h"
+#include "date.h"
+
+
+class DrawHistoryManager: public DrawHistoryManagerGUI
+{
+public:
+  DrawHistoryManager(ResultManager *resultmanager, const std::string &configdir);
+  void updateDashboard(void);
+private:
+  ResultManager *rm;
+  std::string homedir;
+};
+
+#endif
---
+++ src/DrawHistoryManager.cxx
@@ -0,0 +1,52 @@
+#include <sstream>
+
+    
+#include "DrawHistoryManager.h"
+
+DrawHistoryManager::DrawHistoryManager(ResultManager *resultmanager, const std::string &configdir)
+{
+  rm = resultmanager;
+  homedir = configdir;
+}
+
+void DrawHistoryManager::updateDashboard(void)
+{
+  //rm->downloadResults(R_TATTSLOTTO);
+  
+  std::string s;
+  std::stringstream out;
+  std::cout << "&&&" << std::endl;
+  int x = rm->getLastDraw(R_TATTSLOTTO);
+  out << x;
+  s = out.str();
+  this->tatts_last_draw->value(s.c_str()); // FL_ouput makes a copy of the string, so this is OK.
+  out.str("");
+  x = rm->getLastDraw(R_OZLOTTO);
+  out << x;
+  s = out.str();
+  this->ozlotto_last_draw->value(s.c_str());
+  out.str("");
+  x = rm->getLastDraw(R_POWERBALL);
+  out << x;
+  s = out.str();
+  this->powerball_last_draw->value(s.c_str());
+  out.str("");
+  x = rm->getLastDraw(R_SET_FOR_LIFE);
+  out << x;
+  s = out.str();
+  this->sfl_last_draw->value(s.c_str());
+  out.str("");
+  s = rm->getDate(R_TATTSLOTTO);
+  this->tatts_last_date->value(s.c_str());
+  out.str("");
+  std::cout << "&&&" << std::endl;
+  s = rm->getDate(R_OZLOTTO);
+  this->ozlotto_last_date->value(s.c_str());
+  out.str("");
+  s = rm->getDate(R_POWERBALL);
+  this->powerball_last_date->value(s.c_str());
+  out.str("");
+  s = rm->getDate(R_SET_FOR_LIFE);
+  this->sfl_last_date->value(s.c_str());
+
+}
