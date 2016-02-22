21b211b94a9f25285cd5baa49f1a56be79410c53
Dennis Katsonis <dennisk@netspace.net.au>
2016-02-22 20:47:19 +1100

    main() in seperate source code file.

---
+++ src/main.cxx
@@ -0,0 +1,44 @@
+#include <string>
+#include "AussieLotto.h"
+#include "DrawHistoryManager.h"
+#include "ResultManager.h"
+#include "AussieLottoException.h"
+#include "ExceptionHandler.h"
+
+#ifdef __linux
+#include <pwd.h>
+#include <unistd.h>
+#include <sys/types.h>
+#endif
+
+
+int main(void)
+{ 
+  
+  std::string homedir;
+#ifdef __linux
+  passwd* pw = getpwuid(getuid());
+  std::string path(pw->pw_dir);
+  path += "/.config/aussielotto/";
+#endif
+  homedir += path;
+  ResultManager *resultManager;
+  fl_register_images();
+  try {
+    resultManager = new ResultManager(homedir);
+  } catch (AussieLottoException &e) {
+    exceptionHander(e);
+  }
+  
+  AusLotto x(resultManager, homedir);
+  
+  x.make_window();
+  x.lotto_gui->show();
+  x.createOzlottoEntryWindows();
+  x.createTattsEntryWindows();
+  x.createPowerballEntryWindows();
+  x.createLottostrikeEntryWindows();
+  x.createSetForLifeEntryWindows();
+  return Fl::run();
+  delete resultManager;
+}
