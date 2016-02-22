3aaf5fec38753b046a9ae934e041182d0e0184aa
Dennis Katsonis <dennisk@netspace.net.au>
2016-02-22 20:46:18 +1100

    Added Set For Life support.

---
+++ include/SetForLife.h
@@ -0,0 +1,27 @@
+#ifndef SETFORLIFE_H
+#define SETFORLIFE_H
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include "LottoGame.h"
+
+class setforlifeGame : public LottoGame
+{
+  public:
+    int saveGame(std::string fname);
+    int generateGames(int numGames);
+    ~setforlifeGame();
+    setforlifeGame();
+    setforlifeGame(char *fname);
+    setforlifeGame(int numgames);
+    std::string checkResults();
+    
+    friend std::ostream &operator<<(std::ostream &outstream, setforlifeGame & p);
+  private:
+
+};
+
+
+#endif
