fc9320792f89b01338b2737911ea6e50c3ecbf57
Dennis Katsonis <dennisk@netspace.net.au>
2017-09-07 21:44:03 +1000

    Added application icon to the window.

--- src/main.cxx
+++ src/main.cxx
@@ -1,9 +1,12 @@
 #include <string>
+#include <FL/Fl_Pixmap.H> 
+#include <FL/Fl_RGB_Image.H> 
 #include "AussieLotto.h"
 #include "DrawHistoryManager.h"
 #include "ResultManager.h"
 #include "AussieLottoException.h"
 #include "ExceptionHandler.h"
+#include "aussielotto.xpm"
 
 #ifdef __linux
 #include <pwd.h>
@@ -34,11 +37,14 @@ int main(void)
     // Cant run without the result manager.  Bail out.
     return 1;
   }
-  
+  Fl_Pixmap aussielotto_32x32_icon(aussielotto);
+  Fl_RGB_Image aussielotto_icon(&aussielotto_32x32_icon, Fl_Color(0)); 
+ 
   AusLotto x(resultManager, homedir);
-  
   x.make_window();
   x.lotto_gui->show();
+  x.lotto_gui->icon(&aussielotto_icon);
+  x.lotto_gui->xclass("AussieLotto");
   x.createOzlottoEntryWindows();
   x.createTattsEntryWindows();
   x.createPowerballEntryWindows();
